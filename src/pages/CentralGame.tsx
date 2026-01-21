ed8e05b7a7aa894b6fa26a1571c63a21037309b2
João Vitor Hobus <69995779+miojo-dev@users.noreply.github.com>
2026-01-21 09:46:05 -0300

    feat/markCell function

---
+++ src/pages/CentralGame.tsx
@@ -0,0 +1,50 @@
+import {sqr} from "@/lib/utils";
+import {Card, CardContent} from "@/components/ui/card";
+import {Table, TableBody, TableCell} from "@/components/ui/table";
+import {Button} from "@/components/ui/button";
+import {useState} from "react";
+import {Circle, X} from "lucide-react"
+
+export default function CentralGame(
+    props: {
+        className?: string;
+    }) {
+    const [currentSym, setCurrentSym] = useState("x");
+    type CellValue = 'x' | 'o' | null;
+    const [board, setBoard] = useState<CellValue[]>(Array(9).fill(null));
+
+    const markCell = (index: number) => {
+        debugger;
+        if (board[index]) return; // If cell is already marked, do nothing
+        const newBoard = [...board];
+        newBoard[index] = currentSym === 'x' ? 'x' : 'o';
+        setBoard(newBoard);
+        setCurrentSym(currentSym === 'x' ? 'o' : 'x');
+    };
+
+    return (
+        <>
+            <Card className={props.className +"p-0"}>
+                <CardContent >
+                    <Table className="p-0 m-0">
+                        <TableBody>
+                            {board.map((cell, index) => (
+                                <TableCell key={index} className="p-1">
+                                    <Button
+                                        className="w-10 h-10"
+                                        variant="ghost"
+                                        onClick={() => markCell(index)}
+                                        disabled={cell !== null}
+                                    >
+                                        {cell === 'x' ? <X strokeWidth="4px" size="24" className="text-destructive"/> :
+                                            cell === 'o' ? <Circle strokeWidth="4px" size="24" className="text-blue-600"/> : null}
+                                    </Button>
+                                </TableCell>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </CardContent>
+            </Card>
+        </>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,20 @@
 import {AnimatedThemeToggler} from "@/components/ui/animated-theme-toggler";
+import CentralGame from "./CentralGame";
 
 export default function Home() {
     return (
         <>
-            <header className="flex w-full border-b-2 backdrop-blur-sm p-1 justify-between">
+            <header
+                className="flex w-full border-b-2 backdrop-blur-md p-1 justify-between absolute top-0 left-0 right-0">
                 <h1 className="text-2xl font-mono">
                     Tic-Tac-Toe <strong className="font-bold italic font-mono">
-                        Overcharged
-                    </strong> ⭕❌
+                    Overcharged
+                </strong> ⭕❌
                 </h1>
-                <AnimatedThemeToggler className="relative w-8 h-8 "/>
+                <AnimatedThemeToggler className="relative w-8 h-8"/>
             </header>
             <main className="size-full">
-
+                <CentralGame className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"/>
             </main>
         </>
     );
